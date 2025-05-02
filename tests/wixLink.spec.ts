5724370ce1673fa971d9e5f33ecc15706e0025e1
Marium <maryam_cis10@hotmail.com>
2025-05-02 02:50:29 -0500

    Adding more test for weather UI input

--- tests/wixLink.spec.ts
+++ tests/wixLink.spec.ts
@@ -1,7 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Website Title', async ({ page }) => {
-  await page.goto('https://mariumdeu.wixsite.com/assessment');
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://mariumdeu.wixsite.com/assessment");
+});
+
+test("Website Title", async ({ page }) => {
+
+  // Locate title of the page
   const title = await page.title();
-  expect(title).toContain('Weather Condition App');
+  // Assert that title is matching
+  await expect(title).toContain("Weather Condition App");
+});
+
+test("City Label Text", async ({ page }) => {
+
+  // Locate the label with text 'Enter city below'
+  const cityLabel = page.getByText("Enter city below", { exact: true });
+
+  // Assert that the label is visible
+  await expect(cityLabel).toBeVisible();
+});
+
+test("City Input Text", async ({ page }) => {
+   
+  //Locate the text box
+  const cityInput = page.locator("input#input_comp-m9q5vpmm");
+  await expect(cityInput).toHaveAttribute("placeholder", "Enter city name");
+
+  //Type city in text box
+  await cityInput.fill("Chicago");
 });
